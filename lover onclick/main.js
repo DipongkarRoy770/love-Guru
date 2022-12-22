cac1e99833d8fa032cc2730515888bea73856d59
DipongkarRoy <102661635+DipongkarRoy770@users.noreply.github.com>
2022-12-23 01:25:18 +0600

    Add files via upload

---
+++ lover onclick/main.js
@@ -0,0 +1,7 @@
+document.getElementById('btn').addEventListener('click',function(){
+    document.getElementById('text-input').innerText='Delta Computer Science Collage,Rangpur';
+})
+function love(){
+    document.getElementById('text-input').innerText='I Promice ,LOve You Tithi ';
+    
+}
